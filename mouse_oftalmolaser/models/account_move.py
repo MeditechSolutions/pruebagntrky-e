21c15c736183d225d30ad09064e506a9f5f21d91
kquiroz <39870381+kquiroz@users.noreply.github.com>
2020-07-01 18:45:14 -0500

    Add files via upload

---
+++ mouse_oftalmolaser/models/account_move.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError, Warning
+
+class AccountMove(models.Model) :
+    _inherit = 'account.move'
+    
+    def action_post(self) :
+        for record in self.filtered(lambda r: r.company_id.country_id == self.env.ref('base.pe', False)) :
+            if self.env['pos.order'].sudo().search([('account_move','=',record.id)]) and record.partner_id.vat :
+                if len(record.partner_id.vat) == 11 :
+                    diario = self.env['account.journal'].search([('company_id','in',record.company_id.ids),
+                                                                 ('type','=','sale'),
+                                                                 ('l10n_latam_use_documents','!=',False),
+                                                                 ('l10n_latam_document_type_id','=',self.env.ref('mouse_einvoice_base.pe_01').id)], limit=1)
+                    if diario :
+                        record.einvoice_journal_id = diario
+                        record.partner_id.l10n_latam_identification_type_id = self.env.ref('l10n_pe.it_RUC')
+                else :
+                    diario = self.env['account.journal'].search([('company_id','in',record.company_id.ids),
+                                                                 ('type','=','sale'),
+                                                                 ('l10n_latam_use_documents','!=',False),
+                                                                 ('l10n_latam_document_type_id','=',self.env.ref('mouse_einvoice_base.pe_03').id)], limit=1)
+                    if diario :
+                        record.einvoice_journal_id = diario
+                        record.partner_id.l10n_latam_identification_type_id = self.env.ref('l10n_pe.it_DNI')
+        res = super(AccountMove, self).action_post()
+        return res
