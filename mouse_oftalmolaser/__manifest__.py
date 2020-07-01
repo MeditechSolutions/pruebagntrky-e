743cd02467a53279c4d27e7eca5510a5339ea09b
kquiroz <39870381+kquiroz@users.noreply.github.com>
2020-07-01 18:44:56 -0500

    Add files via upload

---
+++ mouse_oftalmolaser/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Oftalmolaser - Odoo',
+    'version' : '13.0.1.0.0',
+    'author' : 'Mouse Technologies',
+    'category' : 'Accounting & Finance',
+    'summary': 'Módulo de personalización para Oftalmolaser.',
+    'license': 'LGPL-3',
+    'description' : """
+Este módulo permite realizar los requisitos solicitados por la empresa Oftalmolaser en Odoo
+""",
+    'website': 'https://www.mouse.pe',
+    'depends' : [
+        'mouse_einvoice_base',
+    ],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    "sequence": 1,
+}
